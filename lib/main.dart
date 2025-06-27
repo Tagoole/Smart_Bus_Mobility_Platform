be3f8f80fa1314f844ed8b4f5ec03145dcd635ea
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-06-28 02:42:37 +0300

    Add or update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:smart_bus_mobility_platform1/routes/app_routes.dart';
 import 'package:smart_bus_mobility_platform1/screens/map_screen.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
 
   if (kIsWeb) {
     await Firebase.initializeApp(
