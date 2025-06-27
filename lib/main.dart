23ce690e57d779542cda0eb1c5727a5e906b09f8
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-06-28 02:19:20 +0300

    Add or update lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:smart_bus_mobility_platform1/routes/app_routes.dart';
+import 'package:smart_bus_mobility_platform1/screens/map_screen.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyAr7u8PTywBlRtm98NuqNzxPNF2w77vp9s", 
+        appId:"1:300946521439:web:127b0355935b896df28aea" , 
+        messagingSenderId: "300946521439", 
+        projectId: "smart-bus-mobility-3f369",
+        storageBucket: "smart-bus-mobility-3f369.firebasestorage.app"
+        )
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Smart Bus Mobility',
+        theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: Colors.blueGrey
+        ),
+        routes: AppRoutes.getRoutes(),
+
+        //initialRoute: AppRoutes.mapScreen,
+
+        home: MapScreen(),
+      ),
+    );
+  }
+}
