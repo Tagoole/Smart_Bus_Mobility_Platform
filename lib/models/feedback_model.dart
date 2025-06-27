5edf580095cb669e29c9fbd0ad1b1d97b3ddb9cc
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-06-28 02:19:28 +0300

    Add or update lib/models/feedback_model.dart

---
+++ lib/models/feedback_model.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FeedbackModel {
+  final String feedbackId;
+  final String userId;
+  final String message;
+  final int rating; // 1 to 5 stars
+  final DateTime timestamp;
+
+  FeedbackModel({
+    required this.feedbackId,
+    required this.userId,
+    required this.message,
+    required this.rating,
+    required this.timestamp,
+  });
+
+  factory FeedbackModel.fromJson(Map<String, dynamic> json, String docId) {
+    return FeedbackModel(
+      feedbackId: docId,
+      userId: json['userId'],
+      message: json['message'],
+      rating: json['rating'],
+      timestamp: (json['timestamp'] as Timestamp).toDate(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'userId':userId,
+      'message':message,
+      'rating':rating,
+      'timestamp':timestamp,
+    };
+  }
+}
