7e877a41227ddbb54af1aed4e24a3db21c76bb41
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-06-28 02:18:55 +0300

    Add or update lib/bloc/ticket_bloc/ticket_state.dart

---
+++ lib/bloc/ticket_bloc/ticket_state.dart
@@ -0,0 +1,10 @@
+part of 'ticket_bloc.dart';
+
+sealed class TicketState extends Equatable {
+  const TicketState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class TicketInitial extends TicketState {}
