71f7d7488e91a3b8c3ef211bf86dabe8af44fc13
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-06-28 14:10:54 +0300

    Add or update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:smart_bus_mobility_platform1/routes/app_routes.dart';
-//import 'package:smart_bus_mobility_platform1/screens/map_screen.dart';
-
+//import 'firebase_options.dart';
+import 'package:smart_bus_mobility_platform1/screens/map_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,20 +13,27 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: FirebaseOptions(
-        apiKey: "AIzaSyAr7u8PTywBlRtm98NuqNzxPNF2w77vp9s", 
-        appId:"1:300946521439:web:127b0355935b896df28aea" , 
-        messagingSenderId: "300946521439", 
+        apiKey: "AIzaSyAr7u8PTywBlRtm98NuqNzxPNF2w77vp9s",
+        appId: "1:300946521439:web:127b0355935b896df28aea",
+        messagingSenderId: "300946521439",
         projectId: "smart-bus-mobility-3f369",
-        storageBucket: "smart-bus-mobility-3f369.firebasestorage.app"
-        )
+        storageBucket: "smart-bus-mobility-3f369.firebasestorage.app",
+      ),
     );
-  } else {
-    await Firebase.initializeApp();
+  } else if (Platform.isAndroid) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyAr7u8PTywBlRtm98NuqNzxPNF2w77vp9s",
+        appId: "1:300946521439:android:1699793b44477bfaf28aea",
+        messagingSenderId: "300946521439",
+        projectId: "smart-bus-mobility-3f369",
+        storageBucket: "smart-bus-mobility-3f369.firebasestorage.app",
+      ),
+    );
   }
 
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -36,21 +45,17 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Smart Bus Mobility',
         theme: ThemeData.light().copyWith(
-          scaffoldBackgroundColor: Colors.blueGrey
+          scaffoldBackgroundColor: Colors.blueGrey,
         ),
         routes: AppRoutes.getRoutes(),
 
         //initialRoute: AppRoutes.mapScreen,
-        //home: MapScreen(),
-        home: Scaffold(
-          backgroundColor: Colors.blue,
-          body: Container(
-            width: 200,
-            height: 200,
-            color: Colors.amberAccent,
-          ),
-        ),
-    )
+        home: MapScreen(),
+        //home: Scaffold(
+        //  backgroundColor: Colors.blue,
+        //  body: Container(width: 200, height: 200, color: Colors.amberAccent),
+        //),
+      ),
     );
   }
 }
