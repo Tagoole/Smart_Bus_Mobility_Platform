50204e48f85c5b921a1c17ba0105fc5327fcdaa2
namujuziritah <nritah2024@gmail.com>
2025-07-01 15:44:49 +0300

    added forgot_password screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:smart_bus_mobility_platform1/routes/app_routes.dart';
+import 'package:smart_bus_mobility_platform1/screens/email_verification_success_screen_animated.dart';
 import 'package:smart_bus_mobility_platform1/screens/login_screen.dart';
 import 'package:smart_bus_mobility_platform1/screens/signup_screen.dart';
 import 'package:smart_bus_mobility_platform1/screens/forgot_password_screen.dart';
-import 'package:smart_bus_mobility_platform1/screens/email_verification_screen.dart'; // Add this import
+import 'package:smart_bus_mobility_platform1/screens/email_verification_screen.dart';
+import 'package:smart_bus_mobility_platform1/screens/forgot_password_screen2.dart';
+// Add this import
 
 //import 'firebase_options.dart';
 import 'package:smart_bus_mobility_platform1/screens/map_screen.dart';
@@ -57,11 +60,14 @@ class MyApp extends StatelessWidget {
           '/signup': (context) => const SignUpScreen(), // Add signup route
           '/signin': (context) => const SignInScreen(), // Add signin route
           '/forgotpassword': (context) => const ForgotPasswordScreen(), // Forgot password route
-          '/emailverification': (context) => const EmailVerificationScreen(), // Email verification route
+          '/emailverification': (context) => const EmailVerificationScreen(),
+          '/emailverificationsuccess': (context) => const EmailVerificationSuccessScreenAnimated(),
+          '/forgotpassword2':(context) => const ForgotPasswordScreen2(),// Email verification success route
+        
         },
                  
         //initialRoute: AppRoutes.mapScreen,
-        home:SignUpScreen(),
+        home:ForgotPasswordScreen2(),
                  
         //home: Scaffold(
         //  backgroundColor: Colors.blue,
