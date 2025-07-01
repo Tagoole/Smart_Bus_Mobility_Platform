9fc0c2c44b1a4e7e7777fbdd7c96931fb383b5b1
Larrie-Tau <kasibantelarry@gmail.com>
2025-07-01 19:14:46 +0300

    Add or update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:smart_bus_mobility_platform1/routes/app_routes.dart';
-import 'package:smart_bus_mobility_platform1/screens/admin_screen.dart';
+import 'package:smart_bus_mobility_platform1/screens/admin_screens/admin_screen.dart';
 import 'package:smart_bus_mobility_platform1/screens/login_screen.dart';
 //import 'firebase_options.dart';
 import 'package:smart_bus_mobility_platform1/screens/map_screen.dart';
+import 'package:smart_bus_mobility_platform1/screens/route_managment.dart';
 import 'package:smart_bus_mobility_platform1/screens/splash_screen.dart';
 
 void main() async {
@@ -53,7 +54,7 @@ class MyApp extends StatelessWidget {
         routes: AppRoutes.getRoutes(),
 
         //initialRoute: AppRoutes.mapScreen,
-        home: AdminDashboardScreen(),
+        home: RouteManagementScreen(),
         //home: Scaffold(
         //  backgroundColor: Colors.blue,
         //  body: Container(width: 200, height: 200, color: Colors.amberAccent),
