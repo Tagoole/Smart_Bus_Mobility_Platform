02dd5883a02ddd1defff58bc5e223df7e975c641
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-07-02 20:59:28 +0300

    Add or update lib/routes/app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smart_bus_mobility_platform1/screens/admin_home_screen.dart';
+import 'package:smart_bus_mobility_platform1/screens/bus_driver_home_screen.dart';
+import 'package:smart_bus_mobility_platform1/screens/customer_home_screen.dart';
 import 'package:smart_bus_mobility_platform1/screens/email_verification_screen.dart';
 import 'package:smart_bus_mobility_platform1/screens/forgot_password_screen.dart';
 import 'package:smart_bus_mobility_platform1/screens/home_screen.dart';
@@ -16,9 +19,9 @@ class AppRoutes {
   static const String signUpScreen = '/signup';
   static const String ticketScreen = '/ticket';
   static const String seatBookingScreen = '/seatbooking';
-  static const String passengerScreen = '/passenger';
+  static const String passengerHomeScreen = '/passenger';
   static const String adminScreen = '/admin';
-  static const String busDriverScreen = '/busdriver';
+  static const String busDriverHomeScreen = '/busdriver';
   static const String emailVerificationScreen = '/verifyEmail';
   static const String forgotPasswordScreen = '/forgotPassword';
 
@@ -26,12 +29,16 @@ class AppRoutes {
   static Map<String, WidgetBuilder> getRoutes() {
     return {
       // Add more routes here
-      homeScreen: (context) => SplashScreen(),
+      //homeScreen: (context) => SplashScreen(),
       signUpScreen: (context) => SignUpScreen(),
       loginScreen: (context) => SignInScreen(),
       mapScreen: (context) => MapScreen(),
       emailVerificationScreen: (context) => EmailVerificationScreen(),
       forgotPasswordScreen: (context) => ForgotPasswordScreen(),
+      adminScreen: (context) => AdminHomeScreen(),
+      passengerHomeScreen: (context) => CustomerHomeScreen(),
+      busDriverHomeScreen: (context) => BusDriverHomeScreen()
+
 
 
 
