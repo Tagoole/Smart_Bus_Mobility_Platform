ebad5c2d170be86553aaacd1061fe3a40800304e
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-07-03 12:05:33 +0300

    Add or update lib/screens/passenger_map_screen.dart

---
+++ lib/screens/passenger_map_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PassengerMapScreen extends StatefulWidget {
+  const PassengerMapScreen({super.key});
+
+  @override
+  State<PassengerMapScreen> createState() => _PassengerMapScreenState();
+}
+
+class _PassengerMapScreenState extends State<PassengerMapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
