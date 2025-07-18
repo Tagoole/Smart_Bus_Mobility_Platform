1aaf7009e7cba61318874843820bc26ec0a5d342
Larrie-Tau <kasibantelarry@gmail.com>
2025-07-18 08:52:35 +0300

    Add or update lib/screens/map_selection_screen.dart

--- lib/screens/map_selection_screen.dart
+++ lib/screens/map_selection_screen.dart
@@ -12,6 +12,7 @@ class MapSelectionScreen extends StatefulWidget {
     required this.onLocationSelected,
   });
 
+
   @override
   State<MapSelectionScreen> createState() => _MapSelectionScreenState();
 }
